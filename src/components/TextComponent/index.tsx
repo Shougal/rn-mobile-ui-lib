0d5cce343d29064f6858bfce482fd6ca590cefd3
shougal <xdq9qa@virginia.edu>
2025-06-26 15:55:05 +0300

    Added style to text component

--- src/components/TextComponent/index.tsx
+++ src/components/TextComponent/index.tsx
@@ -3,9 +3,11 @@
 import { Text as RNText} from "react-native"
 
 import {textProps} from './type'
+import { styles } from "./style";
 export const Text = (props:textProps)=>{
+    const styleText= styles.container
     return(
-        <RNText> {props.content}</RNText>
+        <RNText  style={styleText}> {props.content}</RNText>
 
     );
     
