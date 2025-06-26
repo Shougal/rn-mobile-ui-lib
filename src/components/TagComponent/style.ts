583a23fdbe5314d42039ff0ec6c34ea3d8972e8b
shougal <xdq9qa@virginia.edu>
2025-06-26 11:41:58 +0300

    (feat): style sheet for tag Component

---
+++ src/components/TagComponent/style.ts
@@ -0,0 +1,21 @@
+//
+//  style.ts
+//  
+//
+//  Created by shoug alharbi on 6/26/25.
+//
+
+import { StyleSheet } from "react-native";
+
+export const styles= StyleSheet.create({
+    default: {
+        backgroundColor: '#005A9C'
+
+    },
+    offer: {
+        backgroundColor: '#720e9e'
+    }
+
+
+    
+});
