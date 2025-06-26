898e673de09c6693c2a37717a0942698cf283b7a
shougal <xdq9qa@virginia.edu>
2025-06-26 16:00:05 +0300

    Added specific style to the content containers

---
+++ src/components/DismissableComponent/style.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container:{
+        maxHeight: 139,
+        borderRadius: 16,
+        padding: 8,
+        gap:16,
+       
+        width:'100%',
+        flexDirection: 'row',
+        backgroundColor: '#fff'
+        
+    },
+    contentContainer:{
+        width: '50%'
+    }
+   
+
+})
