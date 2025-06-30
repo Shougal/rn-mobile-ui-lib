546cc1aa8445d72554cb9d440eab3912ccd6fcb8
shougal <xdq9qa@virginia.edu>
2025-06-30 14:22:24 +0300

    feat: made dismiss type independent of components types

--- src/components/DismissBanner/types.ts
+++ src/components/DismissBanner/types.ts
@@ -3,7 +3,34 @@ import { LinkProps } from "../Link/types";
 import { TagProps } from "../Tag/types";
 import { TextProps } from "../Text/types"
 
-export interface DismissableProps extends LinkProps,
-    TagProps, TextProps {
-    onDismiss?: () => void
+export interface DismissableProps  {
+    // link props
+    linkLabel: LinkProps["linkLabel"],
+    onPress : LinkProps["onPress"],
+
+    // tag props
+    tagVariant: TagProps['variant'],
+    tagLabel: TagProps['label'],
+    tagColor?: TagProps['color'],
+
+    // text props
+    textContent: TextProps['content'],
+
+}
+
+export interface checking  {
+    // link props
+    linkLabel: LinkProps["linkLabel"],
+    onPress : LinkProps["onPress"],
+
+    // tag props
+    tagVariant: TagProps['variant'],
+    tagLabel: TagProps['label'],
+    tagColor?: TagProps['color'],
+
+    // text props
+    textContent: TextProps['content'],
+
+
+
 }
